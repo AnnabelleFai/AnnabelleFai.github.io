190ca78c0d121b4d8e3bd872b34ef8ea7b3cd355
AnnabelleFai <annabelle.pj.fai@gmail.com>
2026-02-25 13:15:37 -0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,21 +38,30 @@ const testimonialsModalFunc = function () {
 // custom select variables
 const select = document.querySelector("[data-select]");
 const selectItems = document.querySelectorAll("[data-select-item]");
-const selectValue = document.querySelector("[data-selecct-value]");
+const selectValue = document.querySelector("[data-select-value]");
 const filterBtn = document.querySelectorAll("[data-filter-btn]");
 
-select.addEventListener("click", function () { elementToggleFunc(this); });
+// only run if select exists
+if (select) {
 
-// add event in all select items
-for (let i = 0; i < selectItems.length; i++) {
-  selectItems[i].addEventListener("click", function () {
-
-    let selectedValue = this.innerText.toLowerCase();
-    selectValue.innerText = this.innerText;
-    elementToggleFunc(select);
-    filterFunc(selectedValue);
-
+  select.addEventListener("click", function () {
+    elementToggleFunc(this);
   });
+
+  for (let i = 0; i < selectItems.length; i++) {
+    selectItems[i].addEventListener("click", function () {
+
+      let selectedValue = this.innerText.toLowerCase();
+
+      if (selectValue) {
+        selectValue.innerText = this.innerText;
+      }
+
+      elementToggleFunc(select);
+      filterFunc(selectedValue);
+
+    });
+  }
 }
 
 // filter variables
